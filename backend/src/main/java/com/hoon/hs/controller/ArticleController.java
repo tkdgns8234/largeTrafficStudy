15a3f222915c5d46f45b7a6c0e0ae1dcb86de65a
tkdgns8234 <tkdgns82345@naver.com>
2025-05-31 19:29:34 +0900

    게시글 조회 기능 개발

--- backend/src/main/java/com/hoon/hs/controller/ArticleController.java
+++ backend/src/main/java/com/hoon/hs/controller/ArticleController.java
@@ -6,10 +6,15 @@ import com.hoon.hs.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/boards")
@@ -26,5 +31,18 @@ public class ArticleController {
     @PostMapping("/{boardId}/articles")
     public ResponseEntity<Article> writeArticle(@RequestBody WriteArticleDto writeArticleDto) {
         return ResponseEntity.ok(articleService.writeArticle(writeArticleDto));
+    }
+
+    @GetMapping("/{boardId}/articles")
+    public ResponseEntity<List<Article>> getArticle(@PathVariable Long boardId,
+                                                    @RequestParam(required = false) Long lastId,
+                                                    @RequestParam(required = false) Long firstId) {
+        if (lastId != null) {
+            return ResponseEntity.ok(articleService.getOldArticle(boardId, lastId));
+        }
+        if (firstId != null) {
+            return ResponseEntity.ok(articleService.getNewArticle(boardId, firstId));
+        }
+        return ResponseEntity.ok(articleService.firstGetArticle(boardId));
     }
 }
--- backend/src/main/java/com/hoon/hs/repository/ArticleRepository.java
+++ backend/src/main/java/com/hoon/hs/repository/ArticleRepository.java
@@ -1,12 +1,23 @@
 package com.hoon.hs.repository;
 
 import com.hoon.hs.entity.Article;
-import com.hoon.hs.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+    @Query("SELECT a FROM Article a WHERE a.board.id = :boardId ORDER BY a.createdDate DESC")
+    List<Article> findTop10ByBoardIdOrderByCreatedDateDesc(@Param("boardId") Long boardId);
+
+    @Query("SELECT a FROM Article a WHERE a.board.id = :boardId AND a.id < :articleId ORDER BY a.createdDate DESC")
+    List<Article> findTop10ByBoardIdAndArticleIdLessThanOrderByCreatedDateDesc(@Param("boardId") Long boardId,
+                                                                               @Param("articleId") Long articleId);
+
+    @Query("SELECT a FROM Article a WHERE a.board.id = :boardId AND a.id > :articleId ORDER BY a.createdDate DESC")
+    List<Article> findTop10ByBoardIdAndArticleIdGreaterThanOrderByCreatedDateDesc(@Param("boardId") Long boardId,
+                                                                                  @Param("articleId") Long articleId);
 }
